5ad68fe3069fb9138b3b1a1f2cb35097310ea5ab
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-01-27 15:18:37 -0500

    Add frontend url get request for individual club ids and send data back to frontend to load

--- controllers/index.js
+++ controllers/index.js
@@ -7,5 +7,24 @@ router.use("/api/users",userRoutes)
 const clubRoutes = require("./clubRoutes");
 router.use("/api/clubs",clubRoutes)
 
+router.get('/clubs/:id', async (req, res) => {
+    const clubId = req.params.id;
+    console.log(clubId)
+  
+    try {
+        const response = await fetch(`https://api.football-data.org/v4/teams/${clubId}`, {
+          headers: {
+            'X-Auth-Token': process.env.API_KEY, // Replace with your actual Football Data API key
+          },
+        });
+    
+        const data = await response.json();
+        res.json(data);
+      } catch (error) {
+        console.log('Error fetching club data:', error);
+        res.status(500).send('Error fetching club data');
+      }
+  });
+
 
 module.exports = router;
